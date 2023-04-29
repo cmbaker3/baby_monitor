b64ced7def70f313946f28be6880a95c262ff489
cmbaker3 <123706574+cmbaker3@users.noreply.github.com>
2023-04-29 10:46:07 -0700

    Update sub.py

--- sub.py
+++ sub.py
@@ -15,12 +15,12 @@ def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
     #replace user with your USC username in all subscriptions
     client.subscribe("gtrue/ipinfo")
-    client.subscribe("gtrue/date")
+    #client.subscribe("gtrue/date")
     client.subscribe("gtrue/ctime")
     
     #Add the custom callbacks by indicating the topic and the name of the callback handle
     client.message_callback_add("gtrue/ipinfo", on_message_from_ipinfo)
-    client.message_callback_add("gtrue/date", on_message_from_date)
+    #client.message_callback_add("gtrue/date", on_message_from_date)
     client.message_callback_add("gtrue/ctime", on_message_from_ctime)
 
 """This object (functions are objects!) serves as the default callback for 
@@ -33,8 +33,8 @@ def on_message(client, userdata, msg):
 #Custom message callback.
 def on_message_from_ipinfo(client, userdata, message):
    print("Custom callback  - IP Message: "+message.payload.decode())
-def on_message_from_date(client, userdata, message):
-   print("Custom callback  - Date: "+message.payload.decode())
+#def on_message_from_date(client, userdata, message):
+   #print("Custom callback  - Date: "+message.payload.decode())
 def on_message_from_ctime(client, userdata, message):
    print("Custom callback  - Current Time: "+message.payload.decode())
 
@@ -58,7 +58,8 @@ if __name__ == '__main__':
     The keepalive interval indicates when to send keepalive packets to the 
     server in the event no messages have been published from or sent to this 
     client. If the connection request is successful, the callback attached to
-    `client.on_connect` will be called."""    
+    `client.on_connect` will be called."""
+    #the host = _______ is grace's raspberry pi IP address, change to mine if demo from my computer
     client.connect(host="68.181.32.115", port=11000, keepalive=60)
 
     """In our prior labs, we did not use multiple threads per se. Instead, we
