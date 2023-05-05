5ed9f623ff1ad00ecb26421b0fa0416f9f41d195
20gtrue <79179208+20gtrue@users.noreply.github.com>
2023-05-04 18:42:36 -0700

    Update sub.py

--- sub.py
+++ sub.py
@@ -4,6 +4,9 @@ Run vm_pub.py in a separate terminal on your VM."""
 import paho.mqtt.client as mqtt
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd 
+from scipy import signal
+import pywt
 
 """This function (or "callback") will be executed when this client receives 
 a connection acknowledgement packet response from the server. """
@@ -63,7 +66,20 @@ def on_message_from_ipinfo(client, userdata, message):
         plt.ylabel('Sound Value')
         plt.title('Baby Monitor Sound Data')
         plt.show()
-
+        
+        # Signal Processing: Applying a Fourier Transform
+        FFT =np.fft.fft(sound_data)
+        new_N=int(len(FFT)/2) 
+        f_nat=1
+        new_X = np.linspace(10**-12, f_nat/2, new_N, endpoint=True)
+        new_Xph=1.0/(new_X)
+        FFT_abs=np.abs(FFT)
+        plt.plot(new_Xph,2*FFT_abs[0:int(len(FFT)/2.)]/len(new_Xph),color='black')
+        plt.xlabel('Period ($h$)',fontsize=20)
+        plt.ylabel('Amplitude',fontsize=20)
+        plt.title('(Fast) Fourier Transform Method Algorithm',fontsize=20)
+        plt.grid(True)
+        plt.xlim(0,200)
 
 
 if __name__ == '__main__':
